06a068fe68dd0ba76396b0a44b3c53e29674e6d1
byronlui <byronlui@gmail.com>
2026-08-21 15:37:52 +0100

    nothing

--- page/index.php
+++ page/index.php
@@ -16,7 +16,13 @@
             const name = document.querySelector('input[name="name"]').value;
             alert('Hello, ' + name + '!');
         });    </Javascript>
-        
+        <script>
+            document.querySelector('form').addEventListener('submit', function(event) {
+                event.preventDefault();
+                const name = document.querySelector('input[name="name"]').value;
+                alert('Hello, ' + name + '!');
+            });
+        </script>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $name = htmlspecialchars($_POST["name"]);
